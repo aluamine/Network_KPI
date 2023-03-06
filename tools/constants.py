cf5255b8652712c446229f85bf5e8c96b74eb49d
Ali El Amine <42546959+aluamine@users.noreply.github.com>
2023-03-06 15:53:27 +0100

    Update constants.py

--- tools/constants.py
+++ tools/constants.py
@@ -5,7 +5,7 @@ IP_intermediate_1 = "192.168.20.10"
 IP_intermediate_2 = "10.10.10.142"
 PORT_UDP_1 = 2123
 PORT_UDP_2 = 2152
-URI = "mongodb+srv://5GMED:IRT-5GMED@kpi.lyoevpf.mongodb.net/?retryWrites=true&w=majority"
+URI = ""
 DB_NAME = "KPI"
 COLLECTION_NAME_PACKETS = "GTP_Packets"
 COLLECTION_NAME_DROP = "Dropped_Packets"
